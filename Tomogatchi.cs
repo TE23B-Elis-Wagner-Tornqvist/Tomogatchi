8b49311f40f97f490982039f743edf720079d601
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2025-09-12 12:18:34 +0200

    Tamagotchi

---
+++ Tomogatchi.cs
@@ -0,0 +1,47 @@
+
+public class Tomogatchi
+{
+    private int Hunger;
+    private int Boredom;
+    private List<string> Words;
+    private bool IsAlive;
+    public string Name;
+
+
+    public void Feed()
+    {
+
+    }
+
+    public void Hi()
+    {
+
+    }
+
+    public void Teach(string Words)
+    {
+
+    }
+
+    public void Tick()
+    {
+
+    }
+
+    public void PrintStats()
+    {
+        Console.WriteLine($"");
+    }
+
+    public void GetAlive()
+    {
+
+    }
+
+    private void ReduceBordedom()
+    {
+
+    }
+
+
+}
