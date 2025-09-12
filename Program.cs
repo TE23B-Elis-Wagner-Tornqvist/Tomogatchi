f92249648acdc22007d2666012c4895e1f27b053
Elis Wagner Tornqvist <elis.wagnertornkvist@elev.ga.ntig.se>
2025-09-12 13:10:49 +0200

    Tamagotchi#2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿Tomogatchi MyOwnTamagotchi = new();
+
+Console.WriteLine("Vad vill du att din Tama ska heta?");
+MyOwnTamagotchi.Name = Console.ReadLine() ?? string.Empty;
+Console.WriteLine($"{MyOwnTamagotchi.Name} är ett fint namn!");
+
+Console.Clear();
+
+MyOwnTamagotchi.PrintStats();
+Console.WriteLine($"Vad vill du göra med {MyOwnTamagotchi.Name}");
+Console.WriteLine($"1: Prata med {MyOwnTamagotchi.Name} 2: Mata {MyOwnTamagotchi.Name} 3: Lära {MyOwnTamagotchi.Name} något nytt ord.");
+
+string choice = Console.ReadLine();
+
+if (choice == "1")
+{
+    MyOwnTamagotchi.Hi();
+}
+if (choice == "2")
+{
+    MyOwnTamagotchi.Feed();
+}
+if (choice == "3")
+{
+    
+}
+
+
+
+
+
+// Console.WriteLine("Välj ett ord");
+// string Word = Console.ReadLine();
+// MyOwnTamagotchi.Teach(Word); 
+
+// // pou.Tick();
+
+
+Console.ReadLine(); 
+
--- Tomogatchi.cs
+++ Tomogatchi.cs
@@ -1,4 +1,6 @@
 
+using System.IO.Compression;
+
 public class Tomogatchi
 {
     private int Hunger;
@@ -8,39 +10,57 @@ public class Tomogatchi
     public string Name;
 
 
+    public Tomogatchi()
+    {
+        IsAlive = true;
+    }
+
     public void Feed()
     {
-
+        Console.WriteLine($"{Name} äter en donut och är inte lika hungrig längre!");
+        Hunger -= 1;
     }
 
     public void Hi()
     {
-
+        Console.WriteLine("Hej min kompis!");
+        ReduceBordedom();
     }
 
-    public void Teach(string Words)
+    public void Teach(string Word)
     {
-
+        Words.Add(Word);
+        Console.WriteLine($"{Word}");
+        ReduceBordedom();
     }
 
     public void Tick()
     {
 
+        Hunger++;
+        Boredom++;
+
+          if (Hunger > 10 || Boredom > 10)
+            {
+                IsAlive = false;
+                
+            }
+        
     }
 
     public void PrintStats()
     {
-        Console.WriteLine($"");
+        Console.WriteLine($" {Name} är såhär uttråkad: {Boredom}, såhär hungrig: {Hunger} och den {IsAlive}");
     }
 
     public void GetAlive()
     {
-
+       
     }
 
     private void ReduceBordedom()
     {
-
+        Boredom -= 1;
     }
 
 
